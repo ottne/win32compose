6a210df6ae6056262d576a83c19d21e54280c18a
Danni Otterbach <danni.otterbach@googlemail.com>
2023-11-19 00:35:42 +0100

    Refactor to kotlin buildscript

---
+++ lib/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+    id("org.jetbrains.compose")
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+kotlin {
+    mingwX64("mingw") {
+    }
+    sourceSets {
+        val mingwMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+
+            resources.srcDir("src/mingwMain/resources")
+        }
+        val mingwTest by getting {
+        }
+    }
+}
+
+// Use the following Gradle tasks to run your application:
+// :runReleaseExecutableMingw - without debug symbols
+// :runDebugExecutableMingw - with debug symbols
+
+tasks.named("compileKotlinMingw", KotlinCompilationTask::class.java) {
+    compilerOptions {
+        freeCompilerArgs.addAll(
+                "-Xopt-in=kotlinx.cinterop.ExperimentalForeignApi",
+        )
+    }
+}
